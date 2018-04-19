538adfbe2ca33b83b9df009655ae0330f04dc5a8
lyding <352508204@qq.com>
2018-04-19 17:06:48 +0800

    lyd

---
+++ my-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  	<div @click="handleClick" class="dell">{{message}}</div>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+  	return {
+  		message: 'HelloWorld'
+  	}
+  },
+  methods: {
+  	handleClick () {
+  		this.message = 'ByeByeWorld'
+  	}
+  }
+}
+</script>
+
+<style>
+	.dell {
+		color: red;
+	}
+</style>
+
+
+<!--
+	单文件组件 指的是组件的样式，模版，逻辑都写在一个组件里
+{
+	template
+	data
+}
+-->
